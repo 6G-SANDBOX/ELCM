65a851353f37997de7dbbaf6de2e4caf0d7824aa
InterfazHombreMaquina <xpcibertron@uma.es>
2024-09-25 12:18:48 +0200

    Replace task queue with list for task management and simplify initialization

--- Executor/Tasks/Run/kafka_consumerToInflux.py
+++ Executor/Tasks/Run/kafka_consumerToInflux.py
@@ -4,16 +4,11 @@ import json
 from Helper import utils, Level
 from Settings import KAFKAConfig
 
-# Utility function to manage a queue (cola) for handling control messages
-cola = utils.cola
-
 class KafkaConsummerToInflux(Task):
     # Initialize the Task superclass with necessary parameters
-
     def __init__(self, logMethod, parent, params):
         super().__init__("KAFKA", parent, params, logMethod, None)
-    # Define the rules for expected parameters, including which are mandatory
-
+        # Define the rules for expected parameters, including which are mandatory
         self.paramRules = {
             'ExecutionId': (None, True), # Unique ID for execution, required
             'IP': (None, True), # Kafka server IP address, required
@@ -21,15 +16,14 @@ class KafkaConsummerToInflux(Task):
             'TOPIC': (None, True), # Kafka topic to subscribe to, required
             'MEASUREMENT': (None, True), # InfluxDB measurement name, required
             'STOP': (None, True), # Stop signal key, required
-            'ACCOUNT': (False, True), # Account for authentication, optional
+            'ACCOUNT': (False, True), # Account for authentication, required
             'GROUP_ID': (None, False), # Kafka consumer group ID, optional
             'CERTIFICATES': (None, False), # Path to SSL certificates, optional
-            'ENCRYPTION': (False, True)    # Flag for using SSL/TLS, required  
+            'ENCRYPTION': (False, True)    # Flag for using SSL/TLS, required
         }
 
     def Run(self):
         # Extract necessary parameters from the params dictionary
-
         kafka = KAFKAConfig()
         info = kafka.user
         user = info.get("User", None)
@@ -45,68 +39,41 @@ class KafkaConsummerToInflux(Task):
         encryption = self.params['ENCRYPTION']
         account = self.params['ACCOUNT']
 
-        # Create KafkaConsumer based on authentication and encryption configuration
+        # Create common arguments for KafkaConsumer
+        consumer_args = {
+            'bootstrap_servers': f"{IP_host}:{PORT_host}",
+            'group_id': group_id_opt,
+            'consumer_timeout_ms': 1000,
+            'value_deserializer': lambda x: json.loads(x.decode('utf-8'))
+        }
+
+        # Initialize KafkaConsumer based on authentication and encryption configuration
         try:
-            if not isinstance(encryption, bool):
-                self.Log(Level.ERROR, f"Exception creating KAFKA Consumer: bool_1")
-                return
-            if not isinstance(account, bool):
-                self.Log(Level.ERROR, f"Exception creating KAFKA Consumer: bool_2")
+            # Check if encryption and account are boolean values
+            if not isinstance(encryption, bool) or not isinstance(account, bool):
+                self.Log(Level.ERROR, "Exception creating KAFKA: Invalid type for encryption or account")
                 return
 
-            if encryption and account:
-                consumer = KafkaConsumer(
-                    TOPIC_Host,
-                    bootstrap_servers=IP_host + ":" + PORT_host,
-                    group_id=group_id_opt,
-                    ssl_check_hostname=True,
-                    ssl_cafile=base_path + "ca-cert.pem",
-                    ssl_certfile=base_path + "client-cert-signed.pem",
-                    ssl_keyfile=base_path + "client-key.pem",
-                    sasl_plain_username=user,
-                    sasl_plain_password=password,
-                    sasl_mechanism='PLAIN',
-                    security_protocol='SASL_SSL',
-                    consumer_timeout_ms=1000,
-                    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-                )
+            # Determine the security protocol and additional arguments based on conditions
+            if encryption:
+                consumer_args.update({
+                    'ssl_check_hostname': True,
+                    'ssl_cafile': base_path + "ca-cert.pem",
+                    'ssl_certfile': base_path + "client-cert-signed.pem",
+                    'ssl_keyfile': base_path + "client-key.pem",
+                    'security_protocol': 'SASL_SSL' if account else 'SSL'
+                })
 
-            elif encryption and not account:
-                consumer = KafkaConsumer(
-                    TOPIC_Host,
-                    bootstrap_servers=IP_host + ":" + PORT_host,
-                    group_id=group_id_opt,
-                    security_protocol='SSL',
-                    ssl_check_hostname=True,
-                    ssl_cafile=base_path + "ca-cert.pem",
-                    ssl_certfile=base_path + "client-cert-signed.pem",
-                    ssl_keyfile=base_path + "client-key.pem",
-                    consumer_timeout_ms=1000,
-                    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-                )
-
-            elif not encryption and account:
-                consumer = KafkaConsumer(
-                    TOPIC_Host,
-                    bootstrap_servers=IP_host + ":" + PORT_host,
-                    group_id=group_id_opt,
-                    security_protocol='SASL_PLAINTEXT',
-                    sasl_mechanism='PLAIN',
-                    sasl_plain_username=user,
-                    sasl_plain_password=password,
-                    consumer_timeout_ms=1000,
-                    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-                )
-
-            else:
-                consumer = KafkaConsumer(
-                    TOPIC_Host,
-                    bootstrap_servers=IP_host + ":" + PORT_host,
-                    group_id=group_id_opt,
-                    consumer_timeout_ms=1000,
-                    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-                )
-
+            if account:
+                consumer_args.update({
+                    'sasl_plain_username': user,
+                    'sasl_plain_password': password,
+                    'sasl_mechanism': 'PLAIN',
+                    'security_protocol': 'SASL_PLAINTEXT' if not encryption else consumer_args.get('security_protocol')
+                })
+            
+            # Create the KafkaConsumer only once
+            consumer = KafkaConsumer(TOPIC_Host, **consumer_args)
             self.Log(Level.INFO, f"KAFKA CONSUMER successfully connected")
 
         except Exception as e:
@@ -114,34 +81,23 @@ class KafkaConsummerToInflux(Task):
             self.SetVerdictOnError()
             return
 
-        ready = ""
-
         # Main loop for processing Kafka messages
-        while ready != stop:
-            # Check if there is a stop signal in the queue
-            if not cola.empty():
-                ready = cola.get_nowait()
-                if ready != stop:
-                    cola.put_nowait(ready)
+        while stop not in utils.task_list:
             # Consume messages from Kafka and send them to InfluxDB
-
             for message in consumer:
                 try:
                     data = message.value
-                    
                     flattened_data = utils.flatten_json(data)
-
                     for key, value, timestamp in flattened_data:
-                        
+                        # Convert integer values to float
                         if isinstance(value, int):
                             value = float(value)
                         measurement_data = {key: value}
-                        
+                        # Send the flattened data to InfluxDB
                         utils.send_to_influx(measurement, measurement_data, timestamp, executionId)
-                        
-
                     break
                 except Exception as e:
                     self.Log(Level.ERROR, f"Exception consuming KAFKA messages: {e}")
                     self.SetVerdictOnError()
                     return
+        utils.task_list.remove(stop)
