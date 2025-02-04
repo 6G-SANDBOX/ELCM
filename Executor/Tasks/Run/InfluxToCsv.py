1009eba967438aa36d32a0477539c9afe3187022
InterfazHombreMaquina <clgr@uma.es>
2025-02-04 08:48:51 +0100

    Update Run

--- Executor/Tasks/Run/InfluxToCsv.py
+++ Executor/Tasks/Run/InfluxToCsv.py
@@ -1,34 +1,45 @@
 from Task import Task
 from Helper import Level, influx
 from Settings import Config
+import time
 
 class InfluxToCsv(Task):
     def __init__(self, logMethod, parent, params):
         super().__init__("InfluxToCsv", parent, params, logMethod, None)
-
-        # Define required and optional parameters for both InfluxDB v1 and v2
-        self.paramRules = {
-            'ExecutionId': (None, True)
-        }
+        self.paramRules = {'ExecutionId': (None, True)}
 
     def Run(self):
-
-        config=Config()
-        
+        config = Config()
         url = f"http://{config.InfluxDb.Host}:{config.InfluxDb.Port}"
-        version = influx.InfluxDb.detectInfluxDBVersion(url)  # Get the detected version
+        version = influx.InfluxDb.detectInfluxDBVersion(url)
         execution_id = self.params.get('ExecutionId')
         influx_dir = self.parent.TempFolder
-        
-        common_args = {
-            'influx_dir': influx_dir,
-            'execution_id': execution_id,
-            'url': url
-        }
 
         if version == influx.Versions.V1:
-            influx.InfluxDb.export_influxdb_v1(**common_args, database=config.InfluxDb.Database,user=config.InfluxDb.User,password=config.InfluxDb.Password)
+            while influx.InfluxDb.is_influxdb_v1_receiving_data(url, config.InfluxDb.Database, config.InfluxDb.User, config.InfluxDb.Password, execution_id):
+                time.sleep(1)
+
+            influx.InfluxDb.export_influxdb_v1(
+                influx_dir=influx_dir,
+                execution_id=execution_id,
+                url=url,
+                database=config.InfluxDb.Database,
+                user=config.InfluxDb.User,
+                password=config.InfluxDb.Password
+            )
+
         elif version == influx.Versions.V2:
-            influx.InfluxDb.export_influxdb_v2(**common_args, bucket=config.InfluxDb.Database, token=config.InfluxDb.Token, org=config.InfluxDb.Org)
+            while influx.InfluxDb.is_influxdb_v2_receiving_data(url, config.InfluxDb.Database, config.InfluxDb.Token, config.InfluxDb.Org, execution_id):
+                time.sleep(1)
+
+            influx.InfluxDb.export_influxdb_v2(
+                influx_dir=influx_dir,
+                execution_id=execution_id,
+                url=url,
+                bucket=config.InfluxDb.Database,
+                token=config.InfluxDb.Token,
+                org=config.InfluxDb.Org
+            )
+
         else:
             self.Log(Level.ERROR, "Invalid InfluxDB version. Must be '1' or '2'.")
