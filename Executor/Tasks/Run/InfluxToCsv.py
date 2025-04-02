0924f06c455ed69ca42e54d955a581e237b26d5e
InterfazHombreMaquina <clgr@uma.es>
2025-04-02 12:06:56 +0200

    Update InfluxtoCsv

--- Executor/Tasks/Run/InfluxToCsv.py
+++ Executor/Tasks/Run/InfluxToCsv.py
@@ -5,27 +5,50 @@ from Settings import Config
 class InfluxToCsv(Task):
     def __init__(self, logMethod, parent, params):
         super().__init__("InfluxToCsv", parent, params, logMethod, None)
-        self.paramRules = {'ExecutionId': (None, True)}
+        self.paramRules = {
+            'ExecutionId': (None, True),
+            'Token': (None, False),
+            'Password': (None, False),
+            'User': (None, False),
+            'Url': (None, False),
+            'Port': (None, False),
+            'Database': (None, False)
+        }
 
     def Run(self):
         config = Config()
-        url = f"http://{config.InfluxDb.Host}:{config.InfluxDb.Port}"
+
+        host = self.params.get('Url') or config.InfluxDb.Host
+        port = self.params.get('Port') or config.InfluxDb.Port
+        url = f"http://{host}:{port}"
         version = influx.InfluxDb.detectInfluxDBVersion(url)
         execution_id = self.params.get('ExecutionId')
         influx_dir = self.parent.TempFolder
 
-        common_params = {"influx_dir": influx_dir, "execution_id": execution_id, "url": url}
+        token = self.params.get('Token') or config.InfluxDb.Token
+        password = self.params.get('Password') or config.InfluxDb.Password
+        user = self.params.get('User') or config.InfluxDb.User
+        database = self.params.get('Database') or config.InfluxDb.Database
+
+        common_params = {
+            "influx_dir": influx_dir,
+            "execution_id": execution_id,
+            "url": url
+        }
 
         if version == influx.Versions.V1:
-            influx.InfluxDb.export_influxdb_v1(**common_params, 
-                                               database=config.InfluxDb.Database, 
-                                               user=config.InfluxDb.User, 
-                                               password=config.InfluxDb.Password)
-
+            influx.InfluxDb.export_influxdb_v1(
+                **common_params,
+                database=database,
+                user=user,
+                password=password
+            )
         elif version == influx.Versions.V2:
-            influx.InfluxDb.export_influxdb_v2(**common_params, 
-                                               bucket=config.InfluxDb.Database, 
-                                               token=config.InfluxDb.Token, 
-                                               org=config.InfluxDb.Org)
+            influx.InfluxDb.export_influxdb_v2(
+                **common_params,
+                bucket=database,
+                token=token,
+                org=config.InfluxDb.Org
+            )
         else:
             self.Log(Level.ERROR, "Invalid InfluxDB version. Must be '1' or '2'.")
